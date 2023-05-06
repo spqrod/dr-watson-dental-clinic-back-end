b634318d8b96666ae5cad460cdac631b4f518ae8
rodionion <rodionvh@gmail.com>
2023-05-06 11:19:18 -0300

    Work on Google reCaptcha

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const nodemailer =  require("nodemailer");
 const validator =  require("validator");
 const helmet = require("helmet");
 const dotenv = require("dotenv");
+const axios = require("axios");
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -21,6 +22,7 @@ dotenv.config();
 app.use(express.static('build'));
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 
 const emailUsername = process.env.EMAILUSERNAME;
@@ -53,51 +55,51 @@ const contactEmail = nodemailer.createTransport({
     return cleanString;
 };
 
-app.post("/appointment", (req, res) => {
+app.post("/appointment", async (req, res) => {
 
-    const token = "123asdasdasd12easd";
+    const token = req.body.token;
     const url = `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.GOOGLE_CAPTCHA_SECRET_KEY}&response=${token}`;
-    // const options = {
-    //     method: "GET"
-    // };
 
-    // async function getGoogle() {
-    //     const response = await fetch(url, options);
-    //     const result = await response.json();
-    //     console.log("test");
-    //     console.log(result);
-    //     // res.json({result: "123"});
-    // }
-
-    // getGoogle();
-
-    const date = getSanitizedString(req.body.date);
-    const time = getSanitizedString(req.body.time);
-    const name = getSanitizedString(req.body.name) || "Unknown name";
-    const phone = getSanitizedString(req.body.phone);
-    const message = getSanitizedString(req.body.message);
-    const email = {
-        from: `"${name}" info@drwatsondental.com`,
-        to: "info@drwatsondental.com",
-        subject: "New Appointment",
-        html: `
-            <h1>This is a new appointment from our website.</h1>
-            <p>Contact the patient to confirm the appointment or suggest another time.</p>
-            <p>Date: ${date}.</p>
-            <p>Time: ${time}.</p>
-            <p>Name: ${name}.</p>
-            <p>Phone: ${phone}.</p>
-            <p>Message: ${message}.</p>
-            <p><em>This is an automated message. Do not reply directly in the email.</em></p>
-        `
-    };
-    contactEmail.sendMail(email, (error) => {
-        if (error) {
-            res.json({status: "ERROR WHEN SENDING MESSAGE"});
+    try {
+        const response = await axios.post(url)
+        if (response.data.success) {
+            res.json({status: "success"});
         } else {
-            res.json({status: "Message sent"});
+            res.json({status: "failure"});
         }
-    });
+    } catch (error) {
+        res.json({status: error});
+    }
+
+
+
+    // const date = getSanitizedString(req.body.date);
+    // const time = getSanitizedString(req.body.time);
+    // const name = getSanitizedString(req.body.name) || "Unknown name";
+    // const phone = getSanitizedString(req.body.phone);
+    // const message = getSanitizedString(req.body.message);
+    // const email = {
+    //     from: `"${name}" info@drwatsondental.com`,
+    //     to: "info@drwatsondental.com",
+    //     subject: "New Appointment",
+    //     html: `
+    //         <h1>This is a new appointment from our website.</h1>
+    //         <p>Contact the patient to confirm the appointment or suggest another time.</p>
+    //         <p>Date: ${date}.</p>
+    //         <p>Time: ${time}.</p>
+    //         <p>Name: ${name}.</p>
+    //         <p>Phone: ${phone}.</p>
+    //         <p>Message: ${message}.</p>
+    //         <p><em>This is an automated message. Do not reply directly in the email.</em></p>
+    //     `
+    // };
+    // contactEmail.sendMail(email, (error) => {
+    //     if (error) {
+    //         res.json({status: "ERROR WHEN SENDING MESSAGE"});
+    //     } else {
+    //         res.json({status: "Message sent"});
+    //     }
+    // });
 });
 
 app.post("/contact", (req, res) => {
